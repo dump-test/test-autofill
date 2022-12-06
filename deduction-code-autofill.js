22f0b5e735b4e656e0d5860eb5c78fcecfa9f382
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2022-12-07 04:21:12 +0800

    Added deduction code autofill.

---
+++ deduction-code-autofill.js
@@ -0,0 +1,47 @@
+injectJquery( )
+.then(
+	function( ){
+		const targetOption = (
+			"29"
+		);
+
+		const actualCode = (
+			`
+			$(
+				function( ){
+					var chosenSelectComponent = (
+						$( "#field_deduction_code_chzn" )
+					);
+
+					var targetSelectComponent = (
+						$( "select#field-deduction_code" )
+					);
+
+					if(
+							(
+									chosenSelectComponent
+									.length
+								>	0
+							)
+						&&
+							(
+									targetSelectComponent
+									.length
+								>	0
+							)
+					){
+						$( "option[value='${ targetOption }']", targetSelectComponent )
+						.attr( "selected", "selected" );
+
+						targetSelectComponent.trigger( "liszt:updated.chosen" );
+					}
+				}
+			);
+			`
+		);
+
+		document.documentElement.setAttribute( "onreset", actualCode );
+		document.documentElement.dispatchEvent( new CustomEvent( "reset" ) );
+		document.documentElement.removeAttribute( "onreset" );
+	}
+);
