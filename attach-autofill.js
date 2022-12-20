dd436982d7f5daafbc4dcfc1839d2a86c0962796
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2022-12-21 07:29:32 +0800

    Finished initial implementation for attach autofill.

--- attach-autofill.js
+++ attach-autofill.js
@@ -12,15 +12,71 @@ injectJquery( )
 		const actualCode = (
 			`
 			$(
-				function( ){
+				async	function( ){
+							var selectInputFileComponent = (
+								$( "select#fli_files_multiple_select" )
+							);
 
-				}
+							var accountNameLabelComponent = (
+								$( "" )
+							);
+
+							var accountName = (
+								accountNameLabelComponent.text( ).trim( )
+							);
+
+							var fileKeyVoucher = (
+								accountName.replace( /\W+/g, "_" ) + "_VOUCHER"
+							);
+
+							var fileKeyAPD = (
+								accountName.replace( /\W+/g, "_" ) + "_APD"
+							);
+
+							var voucherFileName = (
+								await	(
+											await	fetch(
+														"https://kvdb.io/${ targetKVDBBucket }/" + fileKeyVoucher,
+														{
+															"headers": (
+																{
+																	Authorization: "Basic ${ accessToken }",
+																}
+															),
+														}
+													)
+										).text( )
+							);
+
+							var APDFileName = (
+								await	(
+											await	fetch(
+														"https://kvdb.io/${ targetKVDBBucket }/" + fileKeyAPD,
+														{
+															"headers": (
+																{
+																	Authorization: "Basic ${ accessToken }",
+																}
+															),
+														}
+													)
+										).text( )
+							);
+
+							selectInputFileComponent.append(
+								$( "<option value='" + voucherFileName + "' selected='selected'>" + voucherFileName + "</option>" )
+							);
+
+							selectInputFileComponent.append(
+								$( "<option value='" + APDFileName + "' selected='selected'>" + APDFileName + "</option>" )
+							);
+						}
 			);
 			`
 		);
 
-		document.documentElement.setAttribute( "oninvalid", actualCode );
-		document.documentElement.dispatchEvent( new CustomEvent( "invalid" ) );
-		document.documentElement.removeAttribute( "oninvalid" );
+		document.documentElement.setAttribute( "onsearch", actualCode );
+		document.documentElement.dispatchEvent( new CustomEvent( "search" ) );
+		document.documentElement.removeAttribute( "onsearch" );
 	}
 );
