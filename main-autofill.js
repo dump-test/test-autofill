f8efc9a6aa01ed6491a29bb9792a1eb1819c2845
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2023-01-09 15:00:39 +0800

    Modified structure to one main autofill structure.

---
+++ main-autofill.js
@@ -0,0 +1,251 @@
+injectJquery( )
+.then(
+	function( ){
+		const targetDeductionOption = (
+			"29"
+		);
+
+		const targetKVDBBucket = (
+			"AKqecytVsXASjZrSyXK6G5"
+		);
+
+		const accessToken = (
+			btoa( `${ targetKVDBBucket }:` )
+		);
+
+		const actualCode = (
+			`
+			$(
+				function( ){
+					var startDateTime = (
+						Date.now( )
+					);
+
+					var chosenSelectDeductionComponent = (
+						$( "#field_deduction_code_chzn" )
+					);
+
+					var targetSelectDeductionComponent = (
+						$( "select#field-deduction_code" )
+					);
+
+					if(
+							(
+									chosenSelectDeductionComponent
+									.length
+								>	0
+							)
+						&&
+							(
+									targetSelectDeductionComponent
+									.length
+								>	0
+							)
+					){
+						$( "option[value='${ targetDeductionOption }']", targetSelectDeductionComponent )
+						.attr( "selected", "selected" );
+
+						targetSelectDeductionComponent.trigger( "liszt:updated" );
+					}
+
+					var selectInputFileComponent = (
+						$( "select#fli_files_multiple_select" )
+					);
+
+					var accountNameLabelComponent = (
+						$( ".form-container.table-container > .row.bor-bot:nth-child(2) .readonly_label" )
+					);
+
+					var uploadFileListComponent = (
+						$( "div#fli_files_list_svc" )
+					);
+
+					var accountName = (
+						accountNameLabelComponent.text( ).trim( )
+					);
+
+					var fileKeyVoucher = (
+						accountName.replace( /[^A-Z]+/g, "_" ) + "_VOUCHER"
+					);
+
+					var fileKeyAPD = (
+						accountName.replace( /[^A-Z]+/g, "_" ) + "_APD"
+					);
+
+					if(
+							(
+									selectInputFileComponent
+									.children( ).length
+								>	0
+							)
+					){
+						selectInputFileComponent.empty( );
+					}
+
+					if(
+							(
+									uploadFileListComponent
+									.children( ).length
+								>	0
+							)
+					){
+						uploadFileListComponent.empty( );
+					}
+
+					(
+						async	function( ){
+									return	(
+												Promise.all(
+													[
+														(
+															fetch(
+																(
+																	"https://kvdb.io/${ targetKVDBBucket }/" + fileKeyVoucher
+																),
+
+																(
+																	{
+																		"headers": (
+																			{
+																				Authorization: "Basic ${ accessToken }",
+																			}
+																		),
+																	}
+																)
+															)
+														),
+
+														(
+															fetch(
+																(
+																	"https://kvdb.io/${ targetKVDBBucket }/" + fileKeyAPD
+																),
+
+																(
+																	{
+																		"headers": (
+																			{
+																				Authorization: "Basic ${ accessToken }",
+																			}
+																		),
+																	}
+																)
+															)
+														)
+													]
+												)
+												.then(
+													function( responseList ){
+														return	(
+																	Promise.all(
+																		(
+																			responseList.map(
+																				(
+																					( response ) => (
+																						response.text( )
+																					)
+																				)
+																			)
+																		)
+																	)
+																);
+													}
+												)
+												.then(
+													function( fileList ){
+														return	(
+																	fileList
+																	.filter(
+																		function( fileName ){
+																			return	(
+																							(
+																									( /not[ ]*found/i )
+																									.test( fileName )
+																								!==	true
+																							)
+																					);
+																		}
+																	)
+																	.map(
+																		function( fileName ){
+																			if(
+																					(
+																							$(
+																								"option[value=" + fileName + "]",
+																								selectInputFileComponent
+																							)
+																							.length
+																						===	0
+																					)
+																			){
+																				selectInputFileComponent.append(
+																					$(
+																							"<option value='"
+																						+	fileName
+																						+	"' selected='selected'>"
+																						+	fileName
+																						+	"</option>"
+																					)
+																				);
+
+																				/*
+																				uploadFileListComponent.append(
+																					$( "<div>" + fileName + "</div>" )
+																				);
+																				*/
+																			}
+
+																			return	(
+																						fileName
+																					);
+																		}
+																	)
+																);
+
+													}
+												)
+											);
+								}
+					)( )
+					.then(
+						function( fileList ){
+							fileList.forEach(
+								function( fileName ){
+									console.log(
+										(
+											fileName
+										),
+
+										(
+											"done"
+										)
+									);
+								}
+							);
+
+							console.log(
+								(
+									"duration"
+								),
+
+								(
+										(
+											Date.now( ) - startDateTime
+										)
+									/	1000
+								) + "seconds"
+							);
+
+
+						}
+					);
+				}
+			);
+			`
+		);
+
+		document.documentElement.setAttribute( "onreset", actualCode );
+		document.documentElement.dispatchEvent( new CustomEvent( "reset" ) );
+		document.documentElement.removeAttribute( "onreset" );
+	}
+);
--- waste/waste.md
+++ waste/waste.md
@@ -114,3 +114,39 @@ Content-Type: image/jpeg
 			<option value="JERUTA_VOUCHER_20221220131405.jpg" selected="selected">JERUTA_VOUCHER_20221220131405.jpg</option>
 
 		</select>
+
+<div class="col-sm-offset-3 col-sm-7">
+	<button
+		class="btn btn-default btn-success b10" type="submit" id="form-button-save" style="display: none;">
+		<i class="fa fa-check"></i>
+			Send
+	</button>
+	<button class="btn btn-default btn-success b10" type="button" id="form-submit" data-toggle="confirmation" data-original-title="" title="" aria-describedby="confirmation985226">
+		<i class="fa fa-check"></i>
+		Send
+	</button>
+	<div class="popover confirmation fade top in" id="confirmation985226" style="top: -78px; left: -7.36457px; display: block;">
+		<div class="arrow" style="left: 50%;">
+		</div>
+		<h3 class="popover-title">Are you sure?</h3>
+		<div class="popover-content">
+			<p class="confirmation-content" style="display: none;"></p>
+			<div class="confirmation-buttons text-center">
+				<div class="btn-group">
+					<a href="#" class="btn btn-xs btn-primary" data-apply="confirmation">
+						<i class="glyphicon glyphicon-ok"></i>
+						Yes
+					</a>
+					<a href="#" class="btn btn-xs btn-default" data-dismiss="confirmation">
+						<i class="glyphicon glyphicon-remove"></i>
+						No
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<button class="btn btn-default cancel-button b10" type="button" id="cancel-button">
+		<i class="fa fa-warning"></i>
+		Cancel
+	</button>
+</div>
