713e6bde14bf78cd8d3b281b10bbba3361fb0da1
Richeve Bebedor <richeve.bebedor@gmail.com>
2026-09-20 12:25:40 +0800

    refactor(main-autofill): clean up commented code and streamline variable declarations

--- main-autofill.js
+++ main-autofill.js
@@ -4,105 +4,43 @@ var mainAutofill = function mainAutofill() {
 $(function() {
     var startDateTime = Date.now();
 
-    /*
-    var targetActorLastNameComponent = (
-        $("table.groceryCrudTable > tbody > tr > td:nth-child(1)")
-    );
+    var deductionListTitleComponent = $("h4.panel-title > a:contains('Deduction List')");
 
-    var targetActorFirstNameComponent = (
-        $("table.groceryCrudTable > tbody > tr > td:nth-child(2)")
-    );
+    var addDeductionControlComponent = $("div.col-sm-12 > div.col-sm-1 > a.btn.btn-default[href*='pensions/fli']");
 
-    var targetActorMiddleNameComponent = (
-        $("table.groceryCrudTable > tbody > tr > td:nth-child(3)")
-    );
-    */
-
-    var deductionListTitleComponent = (
-        $("h4.panel-title > a:contains('Deduction List')")
-    );
-
-    var addDeductionControlComponent = (
-        $(
-            "div.col-sm-12 > div.col-sm-1 > " +
-            "a.btn.btn-default[href*='pensions/fli']"
-        )
-    );
-
-    var actorNameLabelComponent = (
-        $(
-            ".form-container.table-container > " +
-            ".row.bor-bot:nth-child(2) .readonly_label"
-        )
-    );
+    var actorNameLabelComponent = $(".form-container.table-container > .row.bor-bot:nth-child(2) .readonly_label");
 
     var actorName = actorNameLabelComponent.text().trim();
 
-    if (
-        /*
-        targetActorLastNameComponent.length > 0 &&
-        targetActorFirstNameComponent.length > 0 &&
-        targetActorMiddleNameComponent.length > 0
-        */
-        actorNameLabelComponent.length > 0 &&
-        deductionListTitleComponent.length > 0
-    ) {
-        var accountName = (
-            /*
-            [
-                targetActorLastNameComponent.text().trim(),
-                targetActorFirstNameComponent.text().trim(),
-                targetActorMiddleNameComponent.text().trim(),
-            ].join(",")
-            */
-            actorName
-        );
+    if (actorNameLabelComponent.length > 0 && deductionListTitleComponent.length > 0) {
+        var accountName = actorName;
 
-        var fileKeyVoucher = (
-            accountName.replace(/[^A-Z]+/g, "_") + "_VOUCHER"
-        );
+        var fileKeyVoucher = accountName.replace(/[^A-Z]+/g, "_") + "_VOUCHER";
 
-        var fileKeyAPD = (
-            accountName.replace(/[^A-Z]+/g, "_") + "_APD"
-        );
+        var fileKeyAPD = accountName.replace(/[^A-Z]+/g, "_") + "_APD";
 
         Promise.all([
             fetch("http://localhost:7375/service/data/keys/pkbmpc/" + fileKeyVoucher),
             fetch("http://localhost:7375/service/data/keys/pkbmpc/" + fileKeyAPD),
         ])
         .then(function(responseList) {
-            return Promise.all(
-                responseList.map((response) => JSON.parse(response.text())?.value ?? "")
-            );
+            return Promise.all(responseList.map((response) => JSON.parse(response.text())?.value ?? ""));
         })
         .then(function(fileList) {
             fileList.forEach(function(fileName) {
                 console.log(fileName, "done");
             });
 
-            console.log(
-                "file cache duration",
-                ((Date.now() - startDateTime) / 1000) + "seconds"
-            );
+            console.log("file cache duration", ((Date.now() - startDateTime) / 1000) + "seconds");
 
-            if (
-                window.localStorage.getItem("target-actor") !== actorName &&
-                addDeductionControlComponent.length > 0
-            ) {
+            if (window.localStorage.getItem("target-actor") !== actorName && addDeductionControlComponent.length > 0) {
                 addDeductionControlComponent[0].click();
                 window.localStorage.setItem("target-actor", actorName);
-            } else if (
-                window.localStorage.getItem("target-actor") !== actorName
-            ) {
-                console.log(
-                    "localstorage actor",
-                    window.localStorage.getItem("target-actor")
-                );
+
+            } else if (window.localStorage.getItem("target-actor") !== actorName) {
+                console.log("localstorage actor", window.localStorage.getItem("target-actor"));
                 console.log("actor", actorName);
-                console.log(
-                    "addDeductionControlComponent",
-                    addDeductionControlComponent
-                );
+                console.log("addDeductionControlComponent", addDeductionControlComponent);
 
                 setTimeout(function() {
                     location.reload();
@@ -113,102 +51,35 @@ $(function() {
         return;
     }
 
-    /*
-    var actorNameLabelComponent = (
-        $(
-            ".form-container.table-container > " +
-            ".row.bor-bot:nth-child(2) .readonly_label"
-        )
-    );
+    var chosenSelectDeductionComponent = $("#field_deduction_code_chzn");
 
-    var addDeductionControlComponent = (
-        $(
-            "div.col-sm-12 > div.col-sm-1 > " +
-            "a.btn.btn-default[href*='pensions/fli']"
-        )
-    );
+    var targetSelectDeductionComponent = $("select#field-deduction_code");
 
-    var actorName = actorNameLabelComponent.text().trim();
+    var submitButtonComponent = $("button.btn.btn-default.btn-success.b10 #form-button-save[type=submit]");
 
-    if (
-        actorNameLabelComponent.length > 0 &&
-        addDeductionControlComponent.length > 0
-    ) {
-        if (window.localStorage.getItem("target-actor") !== actorName) {
-            addDeductionControlComponent[0].click();
-            window.localStorage.setItem("target-actor", actorName);
-        }
+    if (targetSelectDeductionComponent.length <= 0 && submitButtonComponent.length <= 0) return;
 
-        return;
-    }
-    */
-
-    var chosenSelectDeductionComponent = (
-        $("#field_deduction_code_chzn")
-    );
-
-    var targetSelectDeductionComponent = (
-        $("select#field-deduction_code")
-    );
-
-    var submitButtonComponent = (
-        $(
-            "button.btn.btn-default.btn-success.b10" +
-            "#form-button-save[type=submit]"
-        )
-    );
-
-    if (
-        targetSelectDeductionComponent.length <= 0 &&
-        submitButtonComponent.length <= 0
-    ) {
-        return;
-    }
-
-    if (
-        chosenSelectDeductionComponent.length > 0 &&
-        targetSelectDeductionComponent.length > 0
-    ) {
-        $(
-            "option[value='${targetDeductionOption}']",
-            targetSelectDeductionComponent
-        ).attr("selected", "selected");
+    if (chosenSelectDeductionComponent.length > 0 && targetSelectDeductionComponent.length > 0) {
+        $("option[value='${targetDeductionOption}']", targetSelectDeductionComponent).attr("selected", "selected");
 
         targetSelectDeductionComponent.trigger("liszt:updated");
     }
 
-    var selectInputFileComponent = (
-        $("select#fli_files_multiple_select")
-    );
+    var selectInputFileComponent = $("select#fli_files_multiple_select");
 
-    var accountNameLabelComponent = (
-        $(
-            ".form-container.table-container > " +
-            ".row.bor-bot:nth-child(2) .readonly_label"
-        )
-    );
+    var accountNameLabelComponent = $(".form-container.table-container > .row.bor-bot:nth-child(2) .readonly_label");
 
-    var uploadFileListComponent = (
-        $("div#fli_files_list_svc")
-    );
+    var uploadFileListComponent = $("div#fli_files_list_svc");
 
     var accountName = accountNameLabelComponent.text().trim();
 
-    var fileKeyVoucher = (
-        accountName.replace(/[^A-Z]+/g, "_") + "_VOUCHER"
-    );
+    var fileKeyVoucher = accountName.replace(/[^A-Z]+/g, "_") + "_VOUCHER";
 
-    var fileKeyAPD = (
-        accountName.replace(/[^A-Z]+/g, "_") + "_APD"
-    );
+    var fileKeyAPD = accountName.replace(/[^A-Z]+/g, "_") + "_APD";
 
-    if (selectInputFileComponent.children().length > 0) {
-        selectInputFileComponent.empty();
-    }
+    if (selectInputFileComponent.children().length > 0) selectInputFileComponent.empty();
 
-    if (uploadFileListComponent.children().length > 0) {
-        uploadFileListComponent.empty();
-    }
+    if (uploadFileListComponent.children().length > 0) uploadFileListComponent.empty();
 
     (async function() {
         return Promise.all([
@@ -216,38 +87,16 @@ $(function() {
             fetch("http://localhost:7375/service/data/keys/pkbmpc/" + fileKeyAPD),
         ])
         .then(function(responseList) {
-            return Promise.all(
-                responseList.map((response) => JSON.parse(response.text())?.value ?? "")
-            );
+            return Promise.all(responseList.map((response) => JSON.parse(response.text())?.value ?? ""));
         })
         .then(function(fileList) {
             return fileList
                 .filter(function(fileName) {
-                    return (
-                        (/not[ ]*found/i).test(fileName) !== true
-                    );
+                    return ((/not[ ]*found/i).test(fileName) !== true);
                 })
                 .map(function(fileName) {
-                    if (
-                        $(
-                            "option[value='" + fileName + "']",
-                            selectInputFileComponent
-                        ).length === 0
-                    ) {
-                        selectInputFileComponent.append(
-                            $(
-                                "<option value='" + fileName + "'" +
-                                " selected='selected'>" +
-                                fileName +
-                                "</option>"
-                            )
-                        );
-
-                        /*
-                        uploadFileListComponent.append(
-                            $("<div>" + fileName + "</div>")
-                        );
-                        */
+                    if ($("option[value='" + fileName + "']", selectInputFileComponent).length === 0) {
+                        selectInputFileComponent.append($("<option value='" + fileName + "' selected='selected'>" + fileName + "</option>"));
                     }
 
                     return fileName;
@@ -259,31 +108,17 @@ $(function() {
             console.log(fileName, "done");
         });
 
-        console.log(
-            "form send duration",
-            ((Date.now() - startDateTime) / 1000) + "seconds"
-        );
+        console.log("form send duration", ((Date.now() - startDateTime) / 1000) + "seconds");
 
         submitButtonComponent.click();
     });
 });
 `;
 
-    document.documentElement.setAttribute("onreset", actualCode);
-    document.documentElement.dispatchEvent(new CustomEvent("reset"));
-    document.documentElement.removeAttribute("onreset");
+    var script = document.createElement("script");
+    script.textContent = actualCode;
+    document.documentElement.appendChild(script);
+    script.remove();
 };
 
-/**
- * If this does not work use the injectJquery but you need the injectJquery plugin.
- *
- * For this to work properly you have the following option,
- * 1. Install an automatic jQuery injection plugin for this to work.
- * 2. jQuery must be installed on the target site.
- */
 mainAutofill();
-
-/**
- * This will only work if injectJquery plugin is installed.
- */
-// injectJquery().then(mainAutofill);
